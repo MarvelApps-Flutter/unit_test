34db3ef00312675c0957616217ce08042401df4f
Lalit Sharma <lalit@lalitss090.local>
2022-12-25 17:15:19 +0530

    add splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:loginunittest/view/login_view.dart';
-import 'package:loginunittest/widgets/splash_page.dart';
+import 'package:loginunittest/view/splash_page.dart';
 
 void main() {
   runApp(const MyApp());
